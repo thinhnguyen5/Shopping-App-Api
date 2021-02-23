c4e6b12b47a4b67046b04be0366b69bae1ad5936
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-02-23 20:34:34 +0200

    improve

--- components/products.js
+++ components/products.js
@@ -9,12 +9,7 @@ let products= [
         id: 1,
         title: 'Shirt',
         category: 'Shirt',
-        image: [
-            'https://outdoor-and-country-res.cloudinary.com/image/upload/e_trim:2/bo_8px_solid_white/c_pad,b_white,w_1000,h_1200,f_auto,q_auto/v1540205233/product/186710.jpg',
-            'https://i.insider.com/5daf22344af90918f721dc0b?width=600&format=jpeg&auto=webp',
-            'https://i.insider.com/5daf22344af90918f721dc0b?width=600&format=jpeg&auto=webp',
-            'https://i.insider.com/5daf22344af90918f721dc0b?width=600&format=jpeg&auto=webp'
-        ],
+        image: [],
         price: 10,
         brand: "Nike",
         description: 'nice shirt',
@@ -28,12 +23,7 @@ let products= [
         id: 2,
         title: 'Car',
         category: 'Car',
-        image: [
-            'https://preview2.netcarshow.com/Toyota-C-HR-2017-1600-03.jpg',
-            'https://preview2.netcarshow.com/Toyota-C-HR-2017-1600-03.jpg',
-            'https://preview2.netcarshow.com/Toyota-C-HR-2017-1600-03.jpg',
-            'https://preview2.netcarshow.com/Toyota-C-HR-2017-1600-03.jpg'
-        ],
+        image: [],
         price: 10,
         brand: "BMW",
         description: 'nice car',
@@ -47,12 +37,7 @@ let products= [
         id: 3,
         title: 'Shoe',
         category: 'Sneaker',
-        image: [
-            'https://images.complex.com/complex/images/c_crop,h_705,w_1047,x_12,y_18/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/uzwwpxnquuevgquyugey/adidas-yeezy-451-nuriel-rumored-release-info',
-            'https://images.complex.com/complex/images/c_crop,h_705,w_1047,x_12,y_18/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/uzwwpxnquuevgquyugey/adidas-yeezy-451-nuriel-rumored-release-info',
-            'https://images.complex.com/complex/images/c_crop,h_705,w_1047,x_12,y_18/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/uzwwpxnquuevgquyugey/adidas-yeezy-451-nuriel-rumored-release-info',
-            'https://images.complex.com/complex/images/c_crop,h_705,w_1047,x_12,y_18/c_fill,dpr_auto,f_auto,q_90,w_1400/fl_lossy,pg_1/uzwwpxnquuevgquyugey/adidas-yeezy-451-nuriel-rumored-release-info'
-        ],
+        image: [],
         price: 10,
         brand: "adidas",
         description: 'nice shoe',
@@ -66,12 +51,7 @@ let products= [
         id: 4,
         title: 'clothes',
         category: 'Pant',
-        image: [
-            'https://www.target.com.au/medias/static_content/product/images/full/76/39/A1487639.jpg?impolicy=mobile_hero',
-            'https://www.target.com.au/medias/static_content/product/images/full/76/39/A1487639.jpg?impolicy=mobile_hero',
-            'https://www.target.com.au/medias/static_content/product/images/full/76/39/A1487639.jpg?impolicy=mobile_hero',
-            'https://www.target.com.au/medias/static_content/product/images/full/76/39/A1487639.jpg?impolicy=mobile_hero'
-        ],
+        image: [],
         price: 10,
         brand: "Nike",
         description: 'nice pant',
